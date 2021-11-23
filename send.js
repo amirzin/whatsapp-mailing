8697418c2aa546da4e07771d73665fecb665782b
Мирзин Артур Робертович <artur.mirzin@akbarsdigital.ru>
2021-11-23 14:32:02 +0300

    Рассылка из файлов

--- send.js
+++ send.js
@@ -8,6 +8,15 @@ const logToFile = function(d) { //
     log_file.write(util.format(d) + '\n');
     log_stdout.write(util.format(d) + '\n');
 };
+const phoneNumbers = fs.readFileSync('phoneNumbers.txt').toString().replace(' ', '').split("\n");
+
+let messageText = fs.readFileSync('messageText.txt').toString();
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -15,35 +24,32 @@ const logToFile = function(d) { //
         userDataDir: "./user_data"
     });
 
-    for await (const phoneNumber of [
-        '+79214393060',
-        '+79047673908',
-        '+79033199454'
-    ]) {
+    for await (const phoneNumber of phoneNumbers) {
         try {
-            const page = await browser.newPage();
-            await page.goto(`https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURI('Тест')}`, {
-                waitUntil: 'networkidle2',
-            });
-            try {
-                var submitBtn = await page.waitForSelector('button [data-testid=send]', {visible: true, timeout: 10000});
-            } catch (e) {
-                logToFile(`Не удалось отправить на номер ${phoneNumber}`);
-            }
-
-            if (submitBtn) {
-                await page.focus('button [data-testid=send]');
-                // await page.keyboard.type('\n');
-                logToFile(`Отправлено на номер ${phoneNumber}`);
-                page.on('dialog', async dialog => {
-                    console.log(dialog.message());
-                    await dialog.dismiss();
+            if (phoneNumber) {
+                const page = await browser.newPage();
+                await page.goto(`https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURI(messageText)}`, {
+                    waitUntil: 'networkidle2',
                 });
+                try {
+                    var submitBtn = await page.waitForSelector('button [data-testid=send]', {visible: true, timeout: 25000});
+                } catch (e) {
+                    logToFile(`Не удалось отправить на номер ${phoneNumber}`);
+                }
+
+                if (submitBtn) {
+                    await page.focus('button [data-testid=send]');
+                    await page.keyboard.type('\n');
+                    logToFile(`Отправлено на номер ${phoneNumber}`);
+                    page.on('dialog', async dialog => {
+                        console.log(dialog.message());
+                        await dialog.dismiss();
+                    });
+                }
+
+                await delay(1000);
+                await page.close();
             }
-
-            await page.close();
-
-
         } catch (e) {
 
         }
@@ -53,5 +59,5 @@ const logToFile = function(d) { //
 
     // await page.pdf({ path: 'hn.pdf', format: 'a4' });
 
-    // await browser.close();
+    await browser.close();
 })();
